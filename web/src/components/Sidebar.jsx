bf4563e7d9e9b168c37ad0a9fb7c2d4de2d45b6e
JeffGT <jeff.giltoribio@gmail.com>
2026-05-26 09:10:32 -0500

    configurando ventana orange 2

--- web/src/components/Sidebar.jsx
+++ web/src/components/Sidebar.jsx
@@ -341,8 +341,14 @@ export default function Sidebar({ onLogout }) {
                 '                    if os.path.exists(chrome):\n' +
                 '                        args = [chrome]\n' +
                 '                        if PROXY:\n' +
-                '                                                        proxy_parts = PROXY.split(\':\')\n                            if len(proxy_parts) >= 4:\n                                proxy_url = proxy_parts[2] + \':\' + proxy_parts[3] + \'@\' + proxy_parts[0] + \':\' + proxy_parts[1]\n                            else:\n                                proxy_url = PROXY\nargs.append(\'--proxy-server=http://\' + proxy_url)\n' +
-                '                        args.append(url)\n' +
+                '                            proxy_parts = PROXY.split(\':\')\n' +
+                '                            if len(proxy_parts) >= 4:\n' +
+                '                                import subprocess as _sp\n' +
+                '                                _sp.run([\'cmdkey\', \'/add:\' + proxy_parts[0] + \':\' + proxy_parts[1], \'/user:\' + proxy_parts[2], \'/pass:\' + proxy_parts[3]], capture_output=True)\n' +
+                '                                args.append(\'--proxy-server=http://\' + proxy_parts[0] + \':\' + proxy_parts[1])\n' +
+                '                            else:\n' +
+                '                                args.append(\'--proxy-server=http://\' + PROXY)\n' +
+                '                            args.append(url)\n' +
                 '                        subprocess.Popen(args)\n' +
                 '                    else:\n' +
                 '                        subprocess.Popen([\'start\', url], shell=True)\n' +
